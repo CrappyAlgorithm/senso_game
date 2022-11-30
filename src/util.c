9769de8eda92cc2659f188b71ca89d4c2ce3738d
CrappyAlgorithm <sebstian.steinmeyer@fh-bielefeld.de>
2022-11-30 19:12:33 +0100

    Made hw register pointer const.

--- src/util.c
+++ src/util.c
@@ -1,7 +1,7 @@
 #include "util.h"
 
 bool ms_delay_with_button_callback(u_int64_t msec, color_t (*callback)(void), color_t wanted) {
-    volatile u_int64_t * now = (u_int64_t *) (CLINT_BASE_ADDR + CLINT_MTIME);
+    const volatile u_int64_t * now = (u_int64_t *) (CLINT_BASE_ADDR + CLINT_MTIME);
 	volatile u_int64_t save = *now + msec * CLOCK_FREQUENCY;
     bool wanted_pressed = false;
 	while (*now < save) {
@@ -18,7 +18,7 @@ void ms_delay(u_int64_t msec) {
 }
 
 color_t get_random_led(void) {
-    volatile u_int64_t * now = (u_int64_t *) (CLINT_BASE_ADDR + CLINT_MTIME);
+    const volatile u_int64_t * now = (u_int64_t *) (CLINT_BASE_ADDR + CLINT_MTIME);
     int random = *now % COLOR_COUNT;
     return (color_t) random;
 }
